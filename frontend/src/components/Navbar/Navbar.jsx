d759232b405417d02034ec8fc3b662cb98caa7e1
Akash Sharma <aakash6263264@gmail.com>
2024-04-07 10:32:01 +0530

    adding responsiveness

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
     <>
       <header>
         <div className="nav-container">
-          <div className="logo">
+          <div className="logo" style={{ width: "6dvw", height: "6dvh" }}>
             <img className="nav-logo-icon" src={Logo_icon} alt="small-logo" />
           </div>
           <div className="logo">
