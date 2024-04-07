6a6ad6c58e8d2388903d64f1f3521d5be3976600
Akash Sharma <aakash6263264@gmail.com>
2024-04-07 20:05:12 +0530

    adding responsiveness

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-export const server =
-  " https://12d7-2401-4900-1c7b-9e68-948e-4795-427-d1c0.ngrok-free.app";
+export const server = " https://photos.app.goo.gl/DgswAFhniXKxGoQ59";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
