558fd94c76ea7d02fc0efe2358e966b698d53800
Akash Sharma <aakash6263264@gmail.com>
2024-04-10 23:40:15 +0530

    deploying backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-export const server = "192.168.1.7:4000";
+export const server =
+  "https://nurses-birds-expectations-lisa.trycloudflare.com";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
