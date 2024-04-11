af956743e592f2d259f4666c7d12230ecdeb544f
Akash Sharma <aakash6263264@gmail.com>
2024-04-11 13:41:46 +0530

    deploying backend

--- frontend/src/components/RenderImage/RenderImage.jsx
+++ frontend/src/components/RenderImage/RenderImage.jsx
@@ -1,96 +1,95 @@
-// import toast from "react-hot-toast";
-// import { useEffect, useState } from "react";
-// import DefaultProfile from "/DefaultProfile.jpg";
-// import axios from "axios";
-// import "./RenderImage.css";
+import toast from "react-hot-toast";
+import { useEffect, useState } from "react";
+import DefaultProfile from "/DefaultProfile.jpg";
+import axios from "axios";
+import "./RenderImage.css";
 import { useLocation } from "react-router-dom";
-// import { server } from "../../main";
+import { server } from "../../main";
 
 export const RenderImage = () => {
   const location = useLocation();
-  console.log(location.state.data);
-  // const data = location.state.data;
-  // let photo = data.photo;
+  const data = location.state.data;
+  let photo = data.photo;
 
-  // if (photo) photo = server + photo;
-  // const [selectedImage, setSelectedImage] = useState(photo);
-  // if (!photo) setSelectedImage(DefaultProfile);
-  // // const [flag, setFlag] = useState(false);
-  // let rollno = data.rollno;
-  // let teacherId = data.teacherId;
-  // const handleFormSubmit = async (event) => {
-  //   event.preventDefault();
+  if (photo) photo = server + photo;
+  const [selectedImage, setSelectedImage] = useState(photo);
+  if (!photo) setSelectedImage(DefaultProfile);
+  // const [flag, setFlag] = useState(false);
+  let rollno = data.rollno;
+  let tid = data.teacherid;
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-  //   const formData = new FormData(event.target);
-  //   formData.append("rollno", rollno);
-  //   formData.append("teacherId", teacherId);
-  //   try {
-  //     const res = await axios.post(`${server}/uploadImage`, formData, {
-  //       headers: {
-  //         "Content-Type": "multipart/form-data",
-  //       },
-  //     });
-  //     // console.log();
-  //     setSelectedImage(server + res.data.imagePath);
-  //     toast.success(res.data.message);
-  //   } catch (err) {
-  //     console.error("Error occurred while uploading image:", err);
-  //     toast.error(err.response.data.message);
-  //   }
-  // };
-  // return (
-  //   <div className="p-image">
-  //     <form
-  //       onSubmit={handleFormSubmit}
-  //       style={{ width: "100%", height: "100%" }}
-  //       encType="multipart/form-data"
-  //     >
-  //       <input
-  //         type="file"
-  //         id="choose"
-  //         name="profileImage"
-  //         accept="image/*"
-  //         multiple={false}
-  //         style={{ display: "none" }}
-  //       />
-  //       <div
-  //         className="renderProfile"
-  //         style={{
-  //           background: "white",
-  //           display: "flex",
-  //           flexDirection: "column",
-  //           justifyContent: "space-between",
-  //           width: "100%",
-  //           height: "100%",
-  //           alignItems: "center",
-  //           borderRadius: "7px",
-  //         }}
-  //       >
-  //         {selectedImage && (
-  //           <img
-  //             src={selectedImage}
-  //             alt="img"
-  //             style={{ borderRadius: "50%" }}
-  //           />
-  //         )}
-  //         <div style={{ display: "flex", width: "100%" }}>
-  //           <label
-  //             htmlFor="choose"
-  //             className="s-dash-btn"
-  //             style={{ border: "2px solid gray" }}
-  //           >
-  //             Select Image
-  //           </label>
-  //           <button
-  //             style={{ border: "2px solid gray" }}
-  //             type="submit"
-  //             className="s-dash-btn"
-  //           >
-  //             Upload Image
-  //           </button>
-  //         </div>
-  //       </div>
-  //     </form>
-  //   </div>
-  // );
+    const formData = new FormData(event.target);
+    formData.append("rollno", rollno);
+    formData.append("tid", tid);
+    try {
+      const res = await axios.post(`${server}/uploadImage`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      // console.log();
+      setSelectedImage(server + res.data.imagePath);
+      toast.success(res.data.message);
+    } catch (err) {
+      console.error("Error occurred while uploading image:", err);
+      toast.error(err.response.data.message);
+    }
+  };
+  return (
+    <div className="p-image">
+      <form
+        onSubmit={handleFormSubmit}
+        style={{ width: "100%", height: "100%" }}
+        encType="multipart/form-data"
+      >
+        <input
+          type="file"
+          id="choose"
+          name="profileImage"
+          accept="image/*"
+          multiple={false}
+          style={{ display: "none" }}
+        />
+        <div
+          className="renderProfile"
+          style={{
+            background: "white",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            width: "100%",
+            height: "100%",
+            alignItems: "center",
+            borderRadius: "7px",
+          }}
+        >
+          {selectedImage && (
+            <img
+              src={selectedImage}
+              alt="img"
+              style={{ borderRadius: "50%" }}
+            />
+          )}
+          <div style={{ display: "flex", width: "100%" }}>
+            <label
+              htmlFor="choose"
+              className="s-dash-btn"
+              style={{ border: "2px solid gray" }}
+            >
+              Select Image
+            </label>
+            <button
+              style={{ border: "2px solid gray" }}
+              type="submit"
+              className="s-dash-btn"
+            >
+              Upload Image
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
