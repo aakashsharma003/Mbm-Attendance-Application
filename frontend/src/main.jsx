e73701db0d6dd0d5a9e3d6f6ce42a84c8646ed8d
Akash Sharma <aakash6263264@gmail.com>
2024-04-14 12:02:22 +0530

    deploying again after fixing subject page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-export const server =
-  "https://customers-alternatively-bunny-accomplished.trycloudflare.com";
+export const server = "https://feat-project-really-patches.trycloudflare.com";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
