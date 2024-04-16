7a59b4160fbb3b8ccbaebde0e9f79bc93a9f0fa7
Akash Sharma <aakash6263264@gmail.com>
2024-04-16 19:34:58 +0530

    deploying

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-export const server =
-  "https://retreat-latin-direction-entitled.trycloudflare.com";
+export const server = "https://grown-mini-circles-comm.trycloudflare.com";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
