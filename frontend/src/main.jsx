9e95af87eefa12985eee5d566be0aff35374e3a6
Akash Sharma <aakash6263264@gmail.com>
2024-05-24 12:10:15 +0530

    deploying

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 // "http://localhost:8000"
-export const server = "http://localhost:8000";
+export const server =
+  "https://nurse-travis-impression-neighborhood.trycloudflare.com";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
