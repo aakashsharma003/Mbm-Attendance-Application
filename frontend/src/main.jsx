924a01f53b7d7a1e8f6682c7564fb630d2e44a94
Akash Sharma <aakash6263264@gmail.com>
2024-05-25 00:01:09 +0530

    aa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 // "http://localhost:8000"
-export const server =
-  " https://opponent-eat-pending-employer.trycloudflare.com";
+export const server = "https://opponent-eat-pending-employer.trycloudflare.com";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
