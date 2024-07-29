8d7e1c7fe7787914b1e173b2c20ab93bd3b3333d
Akash Sharma <122847899+aakashsharma003@users.noreply.github.com>
2024-07-29 22:24:17 +0530

    changes for deploying backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 // "http://localhost:8000"
-export const server = "http://localhost:8000";
+export const server = import.meta.env.VITE_APP_BACKEND_URL
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
